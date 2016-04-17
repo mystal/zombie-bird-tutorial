8e65d439ca04652a2f92df7209429ddcc9d5866c
Gabriel Martinez <reitaka@gmail.com>
2016-04-17 13:39:59 -0700

    Add config options and change tabs to spaces

--- android/src/com/kilobolt/zombiebird/AndroidLauncher.java
+++ android/src/com/kilobolt/zombiebird/AndroidLauncher.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.kilobolt.zombiebird.ZBGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ZBGame(), config);
-	}
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new ZBGame(), config);
+    }
 }
