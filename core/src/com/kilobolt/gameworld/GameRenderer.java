a1fb619bfed827eab663488a49b961144faa89ae
Gabriel Martinez <reitaka@gmail.com>
2016-04-17 14:20:50 -0700

    Complete Day 4 of tutorial

---
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -0,0 +1,65 @@
+package com.kilobolt.gameworld;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+public class GameRenderer {
+
+    private GameWorld world;
+    private OrthographicCamera cam;
+    private ShapeRenderer shapeRenderer;
+
+    public GameRenderer(GameWorld world) {
+        this.world = world;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, 136, 204);
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(cam.combined);
+    }
+
+    public void render() {
+        /*
+         * 1. We draw a black background. This prevents flickering.
+         */
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        /*
+         * 2. We draw the Filled rectangle
+         */
+
+        // Tells shapeRenderer to begin drawing filled shapes
+        shapeRenderer.begin(ShapeType.Filled);
+
+        // Chooses RGB Color of 87, 109, 120 at full opacity
+        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+
+        // Draws the rectangle from world (Using ShapeType.Filled)
+        shapeRenderer.rect(world.getRect().x, world.getRect().y,
+                world.getRect().width, world.getRect().height);
+
+        // Tells the shapeRenderer to finish rendering
+        // We MUST do this every time.
+        shapeRenderer.end();
+
+        /*
+         * 3. We draw the rectangle's outline
+         */
+
+        // Tells shapeRenderer to draw an outline of the following shapes
+        shapeRenderer.begin(ShapeType.Line);
+
+        // Chooses RGB Color of 255, 109, 120 at full opacity
+        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+
+        // Draws the rectangle from world (Using ShapeType.Line)
+        shapeRenderer.rect(world.getRect().x, world.getRect().y,
+                world.getRect().width, world.getRect().height);
+
+        shapeRenderer.end();
+    }
+}
