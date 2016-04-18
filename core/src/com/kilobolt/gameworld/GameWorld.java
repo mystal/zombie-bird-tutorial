48ce7c0eaf8b2933be9efa64bf39f07355ed5497
Gabriel Martinez <reitaka@gmail.com>
2016-04-17 20:49:05 -0700

    Complete Day 6 of tutorial

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -1,19 +1,20 @@
 package com.kilobolt.gameworld;
 
-import com.badlogic.gdx.math.Rectangle;
+import com.kilobolt.gameobjects.Bird;
 
 public class GameWorld {
 
-    private Rectangle rect = new Rectangle(0, 0, 17, 12);
+    private Bird bird;
 
-    public void update(float delta) {
-        rect.x++;
-        if (rect.x > 137) {
-            rect.x = 0;
-        }
+    public GameWorld(int midPointY) {
+        bird = new Bird(33, midPointY - 5, 17, 12);
     }
 
-    public Rectangle getRect() {
-        return rect;
+    public void update(float delta) {
+        bird.update(delta);
+    }
+
+    public Bird getBird() {
+        return bird;
     }
 }
--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -4,27 +4,39 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.kilobolt.gameworld.GameRenderer;
 import com.kilobolt.gameworld.GameWorld;
+import com.kilobolt.zbhelpers.InputHandler;
 
 public class GameScreen implements Screen {
 
     private GameWorld world;
     private GameRenderer renderer;
+    private float runTime;
 
     public GameScreen() {
         Gdx.app.log("GameScreen", "Attached");
-        world = new GameWorld();
-        renderer = new GameRenderer(world);
+
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float gameWidth = 136;
+        float gameHeight = screenHeight / (screenWidth / gameWidth);
+
+        int midPointY = (int) (gameHeight / 2);
+
+        world = new GameWorld(midPointY);
+        renderer = new GameRenderer(world, (int) gameHeight, midPointY);
+
+        Gdx.input.setInputProcessor(new InputHandler(world.getBird()));
     }
 
     @Override
     public void render(float delta) {
+        runTime += delta;
         world.update(delta);
-        renderer.render();
+        renderer.render(runTime);
     }
 
     @Override
     public void resize(int width, int height) {
-        Gdx.app.log("GameScreen", "resizing");
     }
 
     @Override
